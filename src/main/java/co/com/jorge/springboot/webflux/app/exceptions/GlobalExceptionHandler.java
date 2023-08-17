576e946bca3071a81acf6b22c8fef96d3a83ea35
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2023-08-16 19:36:36 -0500

    Ajuste de exepciones globales para manejo de content type

--- src/main/java/co/com/jorge/springboot/webflux/app/exceptions/GlobalExceptionHandler.java
+++ src/main/java/co/com/jorge/springboot/webflux/app/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package co.com.jorge.springboot.webflux.app.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,9 +17,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ClassCastException.class)
     @ResponseBody
-    public ResponseEntity<Map<String, Object>> handleDecodingException(ClassCastException ex) {
+    public Mono<ResponseEntity<Map<String, Object>>> handleDecodingException(ClassCastException ex) {
         Map<String, Object> errorMessage = new HashMap<>();
-        errorMessage.put("error", "Payload invalido");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        errorMessage.put("errors", Arrays.asList("Payload invalido"));
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(errorMessage));
     }
 }
