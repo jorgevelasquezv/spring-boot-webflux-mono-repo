014bb3c8ae243d3bdbfddce6a8c1d42891fb6429
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2023-08-16 22:04:22 -0500

    Se implementa uso de Eureka server y Spring Cloud gateway puerta de enlace a los micro servicios

---
+++ spring-boot-webflux-eureka-server/src/main/java/co/com/jorge/springboot/webflux/eureka/app/SpringBootWebfluxEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package co.com.jorge.springboot.webflux.eureka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringBootWebfluxEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxEurekaServerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-webflux-gateway/src/test/java/co/com/jorge/springboot/webflux/gateway/app/SpringBootWebfluxGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jorge.springboot.webflux.gateway.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
